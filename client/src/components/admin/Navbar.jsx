ebf3e85b87aad9c5029080e1a7457a377da80739
Tushar Agarwal <56952465+TusharTechs@users.noreply.github.com>
2023-09-11 11:08:16 +0530

    Update Navbar.jsx

--- client/src/components/admin/Navbar.jsx
+++ client/src/components/admin/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
     if (token) {
       if (jwt_decode(token).role === "admin") {
         axios
-          .get(`http://localhost:3000/admin/me`, {
+          .get(`https://mentor-mosaic-server.vercel.app/admin/me`, {
             headers: {
               authorization: token,
             },
