77b63c5a24cce795fd838aa2043316194ab80101
Tushar Agarwal <56952465+TusharTechs@users.noreply.github.com>
2023-09-11 11:06:04 +0530

    Update CreateCourse.jsx

--- client/src/components/admin/CreateCourse.jsx
+++ client/src/components/admin/CreateCourse.jsx
@@ -19,7 +19,7 @@ function Courses() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = { title, tag, price, imgLink, description, videoUrl, published };
-    const response = await axios.post('http://localhost:3000/admin/courses', data, {
+    const response = await axios.post('https://mentor-mosaic-server.vercel.app/admin/courses', data, {
       headers: {
         authorization: token,
       },
